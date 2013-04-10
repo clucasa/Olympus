7f099a05aae31375611aaaccd9f292c0cacca8b7
unknown <clucasa@bat244-12.eecs.engineering.oregonstate.edu>
2013-04-09 19:58:58 -0700

    forgot a shader

---
+++ Olympus/Skybox.hlsl
@@ -0,0 +1,43 @@
+cbuffer ViewProjMat	: c0
+{
+    float4x4 ViewProj;
+}
+
+cbuffer ObjectMat	: c1
+{
+    float4x4 matFinal;
+}
+
+
+TextureCube cubeMap;
+
+struct VOut
+{
+    float4 posH : SV_POSITION;
+    float3 posL : POSITION;
+};
+
+SamplerState samTriLinearSam
+{
+	Filter = MIN_MAG_MIP_LINEAR;
+	AddressU = Wrap;
+	AddressV = Wrap;
+};
+
+VOut VShader(float3 position : POSITION, float3 normal : NORMAL, float3 tangent : TANGENT, float2 texcoord : TEXCOORD )
+{
+    VOut output;
+	
+	//mul(ViewProj, matFinal)
+
+    output.posH = mul(mul(ViewProj, matFinal), float4(position, 1.0f)).xyww;    // transform the vertex from 3D to 2D
+    output.posL = position;
+
+    return output;
+}
+
+
+float4 PShader(VOut input) : SV_TARGET
+{
+    return cubeMap.Sample(samTriLinearSam, input.posL);
+}
